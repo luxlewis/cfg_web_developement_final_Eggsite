26bbfeea59f249d65cb70bda44393773e846b882
OliviaU-B <53919428+OliviaU-B@users.noreply.github.com>
2019-12-16 00:46:01 +0000

    edited hover to mouseenter

--- js/landing.js
+++ js/landing.js
@@ -2,11 +2,7 @@
 //  $("href").effect( "bounce", {times:3}, 300 );
 //});
 $(document).ready(function() {
-
-
-  $(".grid-content-egg").hover(function() {
-      $(this).effect ("bounce", 1000);
+  $(".grid-content-egg").mouseenter(function() {
+    $(this).effect("bounce", 1000);
   });
-
-
-})
+});
